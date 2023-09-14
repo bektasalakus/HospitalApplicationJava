f5901f7a52e243cf52362457de8a046b22090d0e
bektasalakus <bektasalakuss@icloud.com>
2023-09-14 22:44:36 +0300

    TheEnd

--- src/test/java/HospitalProject/HastaneBilgileri.java
+++ src/test/java/HospitalProject/HastaneBilgileri.java
@@ -6,6 +6,7 @@ public class HastaneBilgileri {
     static final String adres = "Melikgazi / Kayseri";
     static final String telefon= "0312 2362554";
 
+
     private HastaneBilgileri(){
 
     }
